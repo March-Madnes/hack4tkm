f17e75f2f60770ca069085d8b5c733f4babe1166
Harshal patil <harshalmukundapatil@gmail.com>
2024-02-17 22:13:06 +0530

    Refactor FarmTasks component and add new UI elements

--- src/pages/FarmTasks.jsx
+++ src/pages/FarmTasks.jsx
@@ -1,19 +1,15 @@
 import React, { useState } from "react";
 import "./farmTasks.css";
 import { FarmHeader } from "../components/Farm";
+import Tasks from "../library/final.json";
 
 const FarmTasks = () => {
-  const [tasks, setTasks] = useState([
-    { id: 1, title: "Task 1", completed: false },
-    { id: 2, title: "Task 2", completed: false },
-    { id: 3, title: "Task 3", completed: false },
-    // Add more tasks as needed
-  ]);
+  const [tasks, setTasks] = useState(Tasks);
 
   const handleTaskCompletion = (taskId) => {
     setTasks((prevTasks) =>
       prevTasks.map((task) =>
-        task.id === taskId ? { ...task, completed: true } : task
+        task.id === taskId ? { ...task, status: !task.status } : task
       )
     );
   };
@@ -21,28 +17,44 @@ const FarmTasks = () => {
   return (
     <div className="p-3">
       <FarmHeader />
-      <div className="vertical-timeline">
-        {tasks.map((task, index) => (
-          <div
-            key={task.id}
-            className={`timeline-card ${task.completed ? "completed" : ""}`}
-            style={{ opacity: task.completed ? 0.5 : 1 }}
-          >
-            <div>
-              <h2 className="text-2xl font-semibold">{task.title}</h2>
-              <p className="text-xs">Due Date</p>
-              <h3 className="text-xl text-orange-700">12/02/2024</h3>
-              <p>Task description goes here</p>
+
+<div class="flex justify-between mb-1 mt-2">
+  <span class="text-base font-medium">Flowbite</span>
+  <span class="text-sm font-medium">45%</span>
+</div>
+<div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+  <div class="bg-green-600 h-2.5 rounded-full" style={{width: "45%"}}></div>
+</div>
+
+
+    <div class="pt-3 w-full">
+        <div class="">
+                <div class="p-4 md:p-5">
+                    <ol class="relative border-s border-gray-200 ms-3.5 mb-4">   
+                        <li class="mb-4 ms-8 bg-white rounded-3xl shadow px-3.5 py-3">   
+                            <span class="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -start-4 ring-8 ring-white ">
+                                <svg class="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"><path fill="currentColor" d="M6 1a1 1 0 0 0-2 0h2ZM4 4a1 1 0 0 0 2 0H4Zm7-3a1 1 0 1 0-2 0h2ZM9 4a1 1 0 1 0 2 0H9Zm7-3a1 1 0 1 0-2 0h2Zm-2 3a1 1 0 1 0 2 0h-2ZM1 6a1 1 0 0 0 0 2V6Zm18 2a1 1 0 1 0 0-2v2ZM5 11v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 11v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 15v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 15v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 11v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM5 15v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM2 4h16V2H2v2Zm16 0h2a2 2 0 0 0-2-2v2Zm0 0v14h2V4h-2Zm0 14v2a2 2 0 0 0 2-2h-2Zm0 0H2v2h16v-2ZM2 18H0a2 2 0 0 0 2 2v-2Zm0 0V4H0v14h2ZM2 4V2a2 2 0 0 0-2 2h2Zm2-3v3h2V1H4Zm5 0v3h2V1H9Zm5 0v3h2V1h-2ZM1 8h18V6H1v2Zm3 3v.01h2V11H4Zm1 1.01h.01v-2H5v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H5v2h.01v-2ZM9 11v.01h2V11H9Zm1 1.01h.01v-2H10v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM9 15v.01h2V15H9Zm1 1.01h.01v-2H10v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM14 15v.01h2V15h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM14 11v.01h2V11h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM4 15v.01h2V15H4Zm1 1.01h.01v-2H5v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H5v2h.01v-2Z"/></svg>
+                            </span>
+                            <h3 class="flex items-start mb-1 text-lg font-semibold text-gray-900 ">Flowbite Application UI v2.0.0 <span class=" border border-green-400 bg-green-100 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-3xl  ms-3">Complete</span></h3>
+                            <time class="block mb-3 text-sm font-normal leading-none text-gray-500 ">Released on Nov 10th, 2023</time>
+                        </li>
+                        <li class="mb-4 ms-8 bg-white rounded-3xl shadow px-3.5 py-3 border-[1px] border-green-600">   
+                            <span class="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -start-4 ring-8 ring-white ">
+                                <svg class="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"><path fill="currentColor" d="M6 1a1 1 0 0 0-2 0h2ZM4 4a1 1 0 0 0 2 0H4Zm7-3a1 1 0 1 0-2 0h2ZM9 4a1 1 0 1 0 2 0H9Zm7-3a1 1 0 1 0-2 0h2Zm-2 3a1 1 0 1 0 2 0h-2ZM1 6a1 1 0 0 0 0 2V6Zm18 2a1 1 0 1 0 0-2v2ZM5 11v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 11v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 15v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 15v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 11v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM5 15v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM2 4h16V2H2v2Zm16 0h2a2 2 0 0 0-2-2v2Zm0 0v14h2V4h-2Zm0 14v2a2 2 0 0 0 2-2h-2Zm0 0H2v2h16v-2ZM2 18H0a2 2 0 0 0 2 2v-2Zm0 0V4H0v14h2ZM2 4V2a2 2 0 0 0-2 2h2Zm2-3v3h2V1H4Zm5 0v3h2V1H9Zm5 0v3h2V1h-2ZM1 8h18V6H1v2Zm3 3v.01h2V11H4Zm1 1.01h.01v-2H5v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H5v2h.01v-2ZM9 11v.01h2V11H9Zm1 1.01h.01v-2H10v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM9 15v.01h2V15H9Zm1 1.01h.01v-2H10v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM14 15v.01h2V15h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM14 11v.01h2V11h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM4 15v.01h2V15H4Zm1 1.01h.01v-2H5v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H5v2h.01v-2Z"/></svg>
+                            </span>
+                            <h3 class="flex items-start mb-1 text-lg font-semibold text-gray-900 ">Flowbite Application UI v2.0.0 <span class=" border border-blue-400 bg-blue-100 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-3xl  ms-3">Active</span></h3>
+                            <time class="block mb-3 text-sm font-normal leading-none text-gray-500 ">Released on Nov 10th, 2023</time>
+                            <div className="flex w-full justify-end"><button className="px-4 py-2 bg-green-700 text-white rounded-xl flex flex-row justify-center gap-3 items-center">
+                              <p>Upload Image</p>
+                              <img src="/icons/camera-solid.svg" alt="camera" className="w-5 h-5" />
+                            </button></div>
+                        </li>
+                      
+                    </ol>
+                </div>
             </div>
-            <input
-              type="checkbox"
-              className="h-6 w-6"
-              checked={task.completed}
-              onChange={() => handleTaskCompletion(task.id)}
-            />
-          </div>
-        ))}
-      </div>
+</div> 
+
     </div>
   );
 };
