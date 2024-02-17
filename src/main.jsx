cfb841f35d5b9bce2a703237625efe01e6bc553b
Harshal patil <harshalmukundapatil@gmail.com>
2024-02-17 12:35:55 +0530

    Add Recommend page

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import  Farm  from './pages/Farm.jsx';
 import LineChart from './components/Chart.jsx';
 import FarmTasks from "./pages/FarmTasks.jsx";
 import { ReactCam } from './components/ReactCam.jsx';
+import Recommend from './pages/recommend/Recommend.jsx';
 
 const router = createBrowserRouter([
   {
@@ -33,6 +34,10 @@ const router = createBrowserRouter([
     path: "/farm/:id/tasks",
     element: <FarmTasks />,
   },
+  {
+    path: "/recommend",
+    element: <Recommend />,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ src/pages/recommend/Recommend.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import CropCard from '../../components/CropCard'
+
+const Recommend = () => { 
+    return (
+        <div>
+           <div
+            className="w-full rounded-3xl bg-cover bg-center flex flex-col justify-between items-end gap-8 shadow-lg"
+            style={{
+              backgroundImage:
+                "url(https://t3.ftcdn.net/jpg/05/02/18/64/360_F_502186443_Kubg3Wl76uE8BYl1tcAuYYXgGKAaO6r4.jpg)",
+            }}
+          >
+            <h1 className="p-3 text-3xl text-green-700 font-black bg-white rounded-bl-3xl text-right">
+              Alphonsa's Farm
+            </h1>
+            <div className="p-3 bg-white rounded-tl-3xl rounded-br-3xl list-none flex flex-col items-stretch gap-2 text-right">
+              <div className="p-1">
+                <div className="text-xs text-gray-400">Last Updated:</div>
+                <li className="text-green-700 font-semibold">16 Feb 2024</li>
+              </div>
+              <div className="p-1">
+                <div className="text-xs text-gray-400">Soil Status</div>
+                <li className="text-red-700 font-semibold">Severe Underhydration</li>
+              </div>
+            </div>
+          </div>
+          <div className="m-3">
+            < CropCard />
+            < CropCard />
+            < CropCard />
+            < CropCard />
+          </div>
+        </div>
+    )
+}
+
+export default Recommend
