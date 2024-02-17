a24d7bf4edda4cfc37ddb57b84196ac5793aaf69
Harshal patil <harshalmukundapatil@gmail.com>
2024-02-17 11:11:54 +0530

    Refactor ReactCam component UI

--- src/components/ReactCam.jsx
+++ src/components/ReactCam.jsx
@@ -34,21 +34,16 @@ export const ReactCam = () => {
             videoConstraints={videoConstraints}>
 
     {({ getScreenshot }) => (
-        <div className="w-full absolute bottom-6 flex justify-between p-3"><>
-            {imgSrc && (<img src={imgSrc} alt="webcam" className='cam-show h-[100px] w-[100px] rounded-3xl border'/>)}
-            <button className='border-8 border-green rounded-3xl p-5 mx-auto'
+        <div className="w-full absolute bottom-6 flex p-4 gap-10 justify-evenly items-end"><>
+            {imgSrc && (<div className='flex flex-col items-center gap-1'><img src={imgSrc} alt="webcam" className='cam-show h-[50px] w-[50px] rounded-3xl border object-cover'/><p>Preview</p> </div>)}
+            <div className='flex flex-col items-center gap-2'><button className='border-4 border-[#808080] rounded-3xl p-6'
                 onClick={capture}
-            > <svg class="w-10 h-10 text-grey-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 12.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Z"/>
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 3h-2l-.447-.894A2 2 0 0 0 12.764 1H7.236a2 2 0 0 0-1.789 1.106L5 3H3a2 2 0 0 0-2 2v11a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V5a2 2 0 0 0-2-2Z"/>
-            </svg>
-            </button>
-            {imgSrc && (<button className='border-8 border-green rounded-3xl p-5'
+            >
+            </button><p>Capture</p></div>
+            {imgSrc && (<div className='flex flex-col items-center gap-1'><button className=''
                 onClick={handleSave}
-            > <svg class="w-10 h-10 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3"/>
-        </svg>
-            </button>)}
+            > <img src="./icons/upload-solid.svg" alt="webcam" className='cam-show h-[40px] w-[40px] object-cover'/>
+            </button> <p>Upload</p> </div>)}
             </>
       </div>
     )}
